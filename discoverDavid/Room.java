49dfa50e19a88ea795d8ae525f8f61f62a63638c
davidfr3378 <157181930+davidfr3378@users.noreply.github.com>
2025-09-05 13:10:13 -0400

    .bypass

--- discoverDavid/Room.java
+++ discoverDavid/Room.java
@@ -23,3 +23,12 @@ Able to transiion to different maps?
 
 Able to add and remove events or doors
 */
+
+/* Yap
+
+What creates a room? (Mapmaker class?)
+How do you translate from room to event to room? (Specifically event to room? Maybe post every event, a method runs that calls its exitpoint? If the exit point is set beforehand, what comes first?)
+
+
+
+ */
