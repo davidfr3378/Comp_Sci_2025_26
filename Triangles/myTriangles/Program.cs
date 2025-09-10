a2f6a5f05ad1e3b02515701189557ce4ad79a1f2
davidfr3378 <157181930+davidfr3378@users.noreply.github.com>
2025-09-10 13:17:20 -0400

    .bypass

--- Triangles/myTriangles/Program.cs
+++ Triangles/myTriangles/Program.cs
@@ -7,26 +7,53 @@ class Program
         Console.WriteLine("_____________________________________________");
         Console.WriteLine("Welcome to Triangle. ");
         int u = getUserChoice();
-        Console.WriteLine("U:" + u);
+        selectAlgo(u)
 
     }
 
     //Solving
     //SAS
-    public double SAS(float side1, float side2, float angle)
+    public double SAS(double side1, double side2, double angle)
     {
-        return 0.0;
-    }
-    //SSS
-    public double heron(float side1, float side2, float side3)
-    {
+        //convert angle to radians
+        Math.pow(side1, 2) + Math.pow(side2, 2) + 2 * side1 * side2 * Math
         return 0.0;
     }
     //ASS
-    public double AAS(float side, float angle1, float angle2)
+    public double AAS(double side, double angle1, double angle2)
     {
+
         return 0.0;
     }
+
+    //AAA
+    public double AAA(double side, double angle1, double angle2)
+    {
+
+        return 0.0;
+    }
+
+    //SSS
+    public double SSS(double side1, double side2, double side3)
+    {
+        
+        return 0.0;
+    }
+
+    public double heron(double side1, double side2, double side3)
+    {
+        
+        return 0.0;
+    }
+
+    public void selectAlgo(int choice)
+    { 
+        switch()
+        {
+            //
+        }
+    }
+    ///
 
 
     //Utility
@@ -35,20 +62,21 @@ class Program
         bool choiceMade = true;
         while (choiceMade)
         {
-            Console.WriteLine("What type of triangle do you want to get the area of? (Enter: 1/2/3/4)");
+            Console.WriteLine("What type of triangle do you want to get the area of? (Enter: 1/2/3/4/5)");
             Console.WriteLine("\t1. AAA (Angle, Angle, Angle)");
             Console.WriteLine("\t2. AAS (Angle, Angle, Side)");
-            Console.WriteLine("\t2. ASS (Angle, Side, Side)");
-            Console.WriteLine("\t3. SSS (Side, Side, Side)");
+            Console.WriteLine("\t3. ASS (Angle, Side, Side)");
+            Console.WriteLine("\t4. SSS (Side, Side, Side)");
+            Console.WriteLine("\t5. SSS (Side, Side, Side) but with Heron's formula");
 
-            if (int.TryParse(Console.ReadLine(), out int choice))
+            if (int.TryParse(Console.ReadLine(), out int choice) || choice < 1 || choice > 5)
             {
                 choiceMade = false;
                 return choice;
             }
             else
             {
-                Console.WriteLine("Invalid input. (Choices are 1/2/3/4)\n ");
+                Console.WriteLine("Invalid input. (Choices are 1/2/3/4/5)\n ");
             }
         }
         return 0;
