410ed631756c985a884689df66e846a272e6e845
davidfr3378 <157181930+davidfr3378@users.noreply.github.com>
2025-11-13 12:56:46 -0500

    .bypass

--- myDogs/DogInteractionHandler.cs
+++ myDogs/DogInteractionHandler.cs
@@ -9,9 +9,10 @@ public class DogInteractionHandler{
     private void interactionHandler(Dog a, Dog b){
         Console.WriteLine($"{a.Name} and {b.Name} have seen each other");
         
-        if ((a.Aggresion > b.Aggresion) && (a.Hunger > b.Hunger)){
+        if (((a.Aggresion && b.Aggresion) > ) && (a.Hunger > b.Hunger)){
             Console.WriteLine($"{a.Name} is more agressive and hungry. It kills {b.Name}");
         }
+        else if(){}
 
     }
 }
