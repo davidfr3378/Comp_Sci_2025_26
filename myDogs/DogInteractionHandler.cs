4664273c233da029d78c92c52dcd8942184457e6
davidfr3378 <157181930+davidfr3378@users.noreply.github.com>
2025-11-13 14:17:22 -0500

    Enhance dog interaction handler logic.b
    Refactor interaction handling logic for dog encounters.

--- myDogs/DogInteractionHandler.cs
+++ myDogs/DogInteractionHandler.cs
@@ -1,18 +1,29 @@
 using System;
 
-public class DogInteractionHandler{
+public class DogInteractionHandler
+{
 
-    public DogInteractionHandler(Road road){
+    public DogInteractionHandler(Road road)
+    {
         road.OnTwoDogsPresent += interactionHandler;
     }
 
-    private void interactionHandler(Dog a, Dog b){
+    private void interactionHandler(Dog a, Dog b)
+    {
         Console.WriteLine($"{a.Name} and {b.Name} have seen each other");
-        
-        Console.WriteLine(
-            a.aggression > b.aggression ? $"{a.Name} attacks and beats {b.Name}" :
-            a.aggression < b.aggression ? $"{b.Name} attacks and beats {a.Name}" :
-            "A and B are evenly matched"
-        );
+
+        // If either of the dogs has agrresion > 5
+        if ((Math.Max(a.Aggression, b.Aggression)) > 5)
+        {
+            Console.WriteLine(
+                a.Aggression > b.Aggression ? $"{a.Name} attacks and beats {b.Name}" :
+                a.Aggression < b.Aggression ? $"{b.Name} attacks and beats {a.Name}" :
+                "A and B are evenly matched"
+            );
+        }
+        else
+        {
+            Console.WriteLine("They are friendly");
+        }
     }
 }
