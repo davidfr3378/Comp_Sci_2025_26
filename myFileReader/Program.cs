1df3a8775dfdc7570a2b3eeaa8d222cabddbf0bf
davidfr3378 <davidezimafr@gmail.com>
2025-11-17 23:06:16 -0500

    ,bypass

---
+++ myFileReader/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void main(string[] args){
+        string[] linesArr = readLines("file.txt");
+
+    }
+
+    public static string[] readLines(string fileName)
+    {
+        int[] numbers;
+        string[] lines;
+        int count = 0;
+
+        try
+        {
+            lines = File.ReadAllLines("data.txt");
+
+            foreach (var item in lines)
+            {
+                if(int.TryParse(item, out int x))
+                { }
+                else
+                {count += 1;}
+            }
+            
+            if (count > 0)
+                return lines;
+            else
+                numbers = Array.ConvertAll(lines, int.Parse);
+                return numbers;
+
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+            return [];
+        }
+    }
+
+
+}
