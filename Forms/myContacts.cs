19e8da68b85f1597157ad89cc8e9eb7a5e580ca1
davidfr3378 <davidezimafr@gmail.com>
2025-12-16 15:43:41 -0500

    sfseF

---
+++ Forms/myContacts.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+
+namespace MyApp.Namespace
+{
+    public partial class myContacts : Form
+    {
+        public myContacts()
+        {
+            InitializeComponent();
+
+            var bwv = new BlazorWebView()
+            {
+                Dock = DockStyle.Fill,
+                HostPage = "wwwroot/index.html",
+                Services = Startup.Services!,
+                StartPath = "/"
+            };
+
+            bwv.RootComponents.Add<Main>("#app");
+            Controls.Add(bwv);
+        }
+    }
+}
