e3ae467967acda91d765fadaa93db3a6f3ee121a
davidfr3378 <davidezimafr@gmail.com>
2026-01-13 22:22:33 -0500

    stuff

--- myDataAnalyzer/Program.cs
+++ myDataAnalyzer/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**/
+
+public class Program
+{
+    public static void Main(string[] args){
+        Console.WriteLine("");
+    }
+
+
+    ///
+    /// PIPELINE FUNCTIONS
+    /// 
+    
+    public void task(){}
+}
--- myDataAnalyzer/obj/Debug/net9.0/myDataAnalyzer.AssemblyInfo.cs
+++ myDataAnalyzer/obj/Debug/net9.0/myDataAnalyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myDataAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19e8da68b85f1597157ad89cc8e9eb7a5e580ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cada4a7f6cb4005a80c2e59349bc9ff4eb6a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("myDataAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myDataAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
